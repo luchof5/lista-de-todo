35f95df3708fdae435c31a782e1c2e196eb702d2
Luis Furtado <lucho_l.f@hotmail.com>
2023-08-03 20:13:33 -0300

    tareas sincronizadas

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // variables
 const formulario =  document.querySelector('#formulario');
-const listaTweets = document.querySelector('#lista-tareas');
-let tweets = [];
+const listaTareas = document.querySelector('#lista-tareas');
+let tareas = [];
 
 
 // Eventos
@@ -16,7 +16,7 @@ function eventListeners() {
 function agregarTexto(e) {
     e.preventDefault();
 
-    // Donde se ingres el texto
+    // Text area
     const texto = document.querySelector('#miAreaTexto').value;
 
     console.log(texto);
@@ -25,7 +25,22 @@ function agregarTexto(e) {
         mostrarError('No se escribio nada');
         return;
     }
+
+    const textoObj = {
+        id: Date.now(),
+        texto
+    }
+
+    // Agregamos al arreglo
+    tareas = [... tareas, textoObj]
+
+    // Agregamos el Html
+    crearHtml();
+
+    // Reiniciar el Htmal
+    formulario.reset();
 }
+
 
 function mostrarError(error) {
     const mensajeError = document.createElement('p');
@@ -41,3 +56,43 @@ function mostrarError(error) {
         mensajeError.remove();
     }, 3000);
 }
+
+function crearHtml() {
+
+    limpiarHtml();
+
+    if(tareas.length > 0) {
+        tareas.forEach(texto => {
+
+            const li = document.createElement('li');
+            li.innerHTML = texto.texto;
+            // Botón de eliminar
+            const btnElminar = document.createElement('button');
+            btnElminar.innerHTML = 'X';
+            
+            
+            li.appendChild(btnElminar);
+            listaTareas.appendChild(li);
+            
+            btnElminar.onclick = () => {
+                borrarTarea(texto.id)
+            }     
+        });
+    }
+}
+
+// Elminar tarea
+function borrarTarea(id) {
+    tareas = tareas.filter ( texto => texto.id !== id);
+
+    crearHtml();
+}
+
+// Limpiar Html
+function limpiarHtml() {
+    while (listaTareas.firstChild) {
+        listaTareas.removeChild(listaTareas.firstChild);
+    }
+}
+
+
